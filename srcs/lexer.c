c14d8233e4815acaabf4d627d06bf7b98efe7de5
eunhkim <valhalla.host@gmail.com>
2020-06-28 13:49:35 +0900

    fix: tokenize about redirect fd in lexer.c

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -25,8 +25,7 @@ static	int		is_start(char *line, t_lexer_tab *tab)
 		return (FALSE);
 	if (tab->quote || ft_isspace(line[tab->idx]))
 		return (FALSE);
-	if (!(tab->quote))
-		tab->quote = get_quote(line, tab->idx);
+	tab->quote = get_quote(line, tab->idx);
 	if (tab->idx && !ft_isspace(line[tab->idx - 1]))
 		return (FALSE);
 	return (TRUE);
@@ -34,15 +33,26 @@ static	int		is_start(char *line, t_lexer_tab *tab)
 
 static	int		is_end(char *line, t_lexer_tab *tab)
 {
+	int		j;
+
 	if (!line || tab->start == -1 || ft_isspace(line[tab->idx]))
 		return (FALSE);
 	if (line[tab->idx] == tab->quote && tab->start != tab->idx)
 		tab->quote = 0;
-	if (line[tab->idx + 1] && (!ft_isspace(line[tab->idx + 1])))
+	if (!line[tab->idx + 1])
+		return (TRUE);
+	if (tab->quote)
 		return (FALSE);
-	if (line[tab->idx + 1] && tab->quote)
+	if (ft_isspace(line[tab->idx + 1] || ft_isset(line[tab->idx + 1], "><")))
+		return (TRUE);
+	if (!ft_isnum(line[tab->idx + 1]))
 		return (FALSE);
-	return (TRUE);
+	j = 2;
+	while (ft_isnum(line[tab->idx + j]))
+		j++;
+	if (ft_isset(line[tab->idx + j], "><"))
+		return (TRUE);
+	return (FALSE);
 }
 
 char			**lexer(char *line)
