647b7934a364ddd1e0f3617ab46337e153aea7b3
humblego <injun.woo30000@gmail.com>
2020-07-07 22:55:00 +0900

    ADD: signal handler of child

--- srcs/signal.c
+++ srcs/signal.c
@@ -1,5 +1,24 @@
 #include "minishell.h"
 
+//TODO: make core dump when get SIGQUIT
+//TODO: test with child process
+void	signal_handler_of_child(int signo)
+{
+	if (signo == SIGINT)
+	{
+		ft_putstr_fd("\n", 1);
+		display_prompt();
+		signal(SIGINT, (void *)signal_handler);
+		exit(0);
+	}
+	else if (signo == SIGQUIT)
+	{
+		ft_putstr_fd("\^\\Quit: 3\n", 2);
+		signal(SIGQUIT, (void *)signal_handler);
+		exit(-1);
+	}
+}
+
 void	signal_handler(int signo)
 {
 	if (signo == SIGINT)
