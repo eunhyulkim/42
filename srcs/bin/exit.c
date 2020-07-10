5479b47c1ba1c9bae6153dce0d989bbb4aac87db
iwoo <iwoo@student.42seoul.kr>
2020-07-10 22:46:56 +0900

    REFACTOR: export.c and error.c

--- srcs/bin/exit.c
+++ srcs/bin/exit.c
@@ -6,7 +6,7 @@
 /*   By: eunhkim <eunhkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 17:03:11 by eunhkim           #+#    #+#             */
-/*   Updated: 2020/07/10 21:58:01 by iwoo             ###   ########.fr       */
+/*   Updated: 2020/07/10 22:37:26 by iwoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ void			cmd_exit(t_command *command)
 	if (command->arg_list)
 	{
 		if (!ft_isnumber(command->arg_list[0]))
-			error_builtin("exit", command->arg_list[0], NUMERIC_REQUIRED);	
+			error_builtin("exit", command->arg_list[0], NUMERIC_REQUIRED);
 		else if (ft_len_doublestr(command->arg_list) > 1)
 			return (error_builtin("exit", "\b\b  \b\b", TOO_MANY_ARG));
 		else
--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: eunhkim <eunhkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 19:41:18 by eunhkim           #+#    #+#             */
-/*   Updated: 2020/07/10 22:11:57 by iwoo             ###   ########.fr       */
+/*   Updated: 2020/07/10 22:46:31 by iwoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,19 +40,18 @@ void	error_builtin(char *cmd, char *arg, char *msg)
 	error_num = errno;
 	ft_putstr_fd(SHELL, 2);
 	ft_putstr_fd(cmd, 2);
-	if (!strcmp(cmd, "unset"))
+	if (!strcmp(cmd, "unset") || !strcmp(cmd, "export"))
 		ft_putstr_fd(": '", 2);
 	else
 		ft_putstr_fd(": ", 2);
 	ft_putstr_fd(arg, 2);
-	if (!strcmp(cmd, "env") || !strcmp(cmd, "pwd") || !strcmp(cmd, "exit")
-			|| !strcmp(cmd, "unset"))
-		ft_putendl_fd(msg, 2);
-	else
+	if (!strcmp(cmd, "cd") || !strcmp(cmd, "echo"))
 	{
 		ft_putstr_fd(": ", 2);
 		ft_putendl_fd(strerror(error_num), 2);
 	}
+	else
+		ft_putendl_fd(msg, 2);
 	set_res(1);
 	return ;
 }
