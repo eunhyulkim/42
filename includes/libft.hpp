08b0efea1d516e3570f87dab890e9f2f0fb3ce1e
eunhkim <valhalla.host@gmail.com>
2020-09-18 23:19:03 +0900

    add libft

---
+++ includes/libft.hpp
@@ -0,0 +1,13 @@
+#ifndef LIBFT_HPP
+# define LIBFT_HPP
+
+# include <string>
+# include <iostream>
+# include <fcntl.h>
+# include <unistd.h>
+
+namespace ft {
+    std::string getStringFromFile(std::string file_path);
+}
+
+#endif
---
+++ srcs/libft.cpp
@@ -0,0 +1,19 @@
+#include "libft.hpp"
+
+namespace ft
+{
+	std::string
+	getStringFromFile(std::string file_path)
+	{
+		int fd = -1;
+		int read_cnt = 0;
+		char buff[1024];
+		std::string ret;
+
+		if ((fd = open(file_path.c_str(), O_RDONLY)) == -1)
+			return (std::string(""));
+		while ((read_cnt = read(fd, buff, 1024)) > 0)
+			ret.append(buff, read_cnt);
+		return (ret);
+	}
+}
