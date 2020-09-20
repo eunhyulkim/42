c1cb3f089f5e2f3c3fa5ce22833c37109caffe3e
Ungchi <dd1360@naver.com>
2020-09-20 22:56:14 +0900

    Add: Server class Constructor and data

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -3,6 +3,15 @@
 
 # include <string>
 # include <iostream>
+# include <map>
+# include <vector>
+# include <queue>
+# include "Location.hpp"
+# include "ServerManager.hpp"
+# include "Connection.hpp"
+# include "Response.hpp"
+# include "Config.hpp"
+# include "Request.hpp"
 
 class Server
 {
@@ -16,37 +25,58 @@ class Server
 		int m_request_header_limit_size;
 		int m_limit_client_body_size;
 		std::string m_default_error_page;
+		Config* m_config;
 		std::vector<Location> m_locations;
 		std::map<int, Connection> m_connections;
 		std::queue<Response> m_responses;
 	public:
 		Server();
-		Server(const std::string& server_block, const std::string& location_blocks);
+		Server(ServerManager* server_manager, int server_fd, const std::string& server_block, std::vector<std::string>& location_blocks, Config* config);
 		Server(const Server& copy);
 		Server& operator=(const Server& obj);
 		virtual ~Server();
 
 		/* getter function */
-		std::string get_m_server_name() const;
-		std::string get_m_host() const;
-		int get_m_port() const;
-		int get_m_fd() const;
-		int get_m_request_uri_limit_size() const;
-		int get_m_request_header_limit_size() const;
-		int get_m_limit_client_body_size() const;
-		std::string get_m_default_error_page() const;
+		// std::string get_m_server_name() const;
+		// std::string get_m_host() const;
+		// int get_m_port() const;
+		// int get_m_fd() const;
+		// int get_m_request_uri_limit_size() const;
+		// int get_m_request_header_limit_size() const;
+		// int get_m_limit_client_body_size() const;
+		// std::string get_m_default_error_page() const;
 
 		/* setter function */
-		void set_m_server_name(std::string server_name);
-		void set_m_host(std::string host);
-		void set_m_port(int port);
-		void set_m_fd(int fd);
-		void set_m_request_uri_limit_size(int request_uri_limit_size);
-		void set_m_request_header_limit_size(int request_header_limit_size);
-		void set_m_limit_client_body_size(int limit_client_body_size);
-		void set_m_default_error_page(std::string default_error_page);
+		// void set_m_server_name(std::string server_name);
+		// void set_m_host(std::string host);
+		// void set_m_port(int port);
+		// void set_m_fd(int fd);
+		// void set_m_request_uri_limit_size(int request_uri_limit_size);
+		// void set_m_request_header_limit_size(int request_header_limit_size);
+		// void set_m_limit_client_body_size(int limit_client_body_size);
+		// void set_m_default_error_page(std::string default_error_page);
 
 		/* declare member function */
+		bool hasException(int client_fd);
+		int closeConnection(int client_fd);
+		int isSendable(int client_fd);
+		int sendResponse(Response response);
+		bool hasRequest(int client_fd);
+		Request readRequest(int client_fd);
+		int solveRequest(Request request);
+		int executeGet(Request request);
+		int executeHead(Request request);
+		int executePut(Request request);
+		int executePost(Request request);
+		int executeDelete(Request request);
+		int executeOptions(Request request);
+		int executeTrace(Request request);
+		char** createCGIEnv(Request request);
+		int executeCGI(Request request);
+		int createResponse(int status);
+		bool hasNewConnection();
+		int acceptNewConnection();
+		int run();
 };
 
 /* global operator overload */
--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -10,30 +10,59 @@
 /* ************************************************************************** */
 
 Server::Server() {}
-Server::Server(const std::string& server_block, const std::string& location_blocks)
-: 
+Server::Server(ServerManager* server_manager, int server_fd, const std::string& server_block, std::vector<std::string>& location_blocks, Config* config)
 {
-	this->m_server_name.clear();
-	this->m_host.clear();
-	this->m_port = 0;
-	this->m_fd = 0;
-	this->m_request_uri_limit_size = 0;
-	this->m_request_header_limit_size = 0;
-	this->m_limit_client_body_size = 0;
-	this->m_default_error_page.clear();
+	std::map<std::string, std::string> server_map = ft::stringVectorToMap(ft::split(server_block, '\n'), ' ');
+	std::string uri;
+	std::string block;
+
+	m_manager = server_manager;
+	m_config = config;
+	m_fd = server_fd;
+	m_server_name = server_map["server_name"];
+	m_host = server_map["host"];
+	m_port = std::stoi(server_map["port"]);
+	m_request_uri_limit_size = std::stoi(server_map["REQUEST_URI_LIMIT_SIZE"]);
+	m_request_header_limit_size = std::stoi(server_map["REQUEST_HEADER_LIMIT_SIZE"]);
+	m_limit_client_body_size = std::stoi(server_map["LIMIT_CLIENT_BODY_SIZE"]);
+	m_default_error_page = server_map["DEFAULT_ERROR_PAGE"];
+	for (std::vector<std::string>::iterator it = location_blocks.begin(); it != location_blocks.end(); ++it)
+	{
+		uri = ft::split(ft::split(*it).front(), ' ')[1];
+		block = *it;
+		block = block.substr(block.find('\n'));
+		m_locations.push_back(Location(uri, block));
+	}
+
+	// test
+	std::cout << m_server_name << std::endl;
+	std::cout << m_host << std::endl;
+	std::cout << m_port << std::endl;
+	std::cout << m_request_uri_limit_size << std::endl;
+	std::cout << m_request_header_limit_size << std::endl;
+	std::cout << m_limit_client_body_size << std::endl;
+	std::cout << m_default_error_page << std::endl;
+	std::cout << "------------------" << std::endl;
+	std::cout << m_locations.front() << std::endl;
+	std::cout << "------------------" << std::endl;
+	std::cout << m_locations.back() << std::endl;
 }
 
 Server::Server(const Server& copy)
-: 
 {
-	this->m_server_name.clear();
-	this->m_host.clear();
-	this->m_port = 0;
-	this->m_fd = 0;
-	this->m_request_uri_limit_size = 0;
-	this->m_request_header_limit_size = 0;
-	this->m_limit_client_body_size = 0;
-	this->m_default_error_page.clear();
+	m_manager = copy.m_manager;
+	m_server_name = copy.m_server_name;
+	m_host = copy.m_host;
+	m_port = copy.m_port;
+	m_fd = copy.m_fd;
+	m_request_uri_limit_size = copy.m_request_uri_limit_size;
+	m_request_header_limit_size = copy.m_request_header_limit_size;
+	m_limit_client_body_size = copy.m_limit_client_body_size;
+	m_default_error_page = copy.m_default_error_page;
+	m_config = copy.m_config;
+	m_locations = copy.m_locations;
+	m_connections = copy.m_connections;
+	m_responses = copy.m_responses;
 }
 
 /* ************************************************************************** */
@@ -42,14 +71,7 @@ Server::Server(const Server& copy)
 
 Server::~Server()
 {
-	this->m_server_name.clear();
-	this->m_host.clear();
-	this->m_port = 0;
-	this->m_fd = 0;
-	this->m_request_uri_limit_size = 0;
-	this->m_request_header_limit_size = 0;
-	this->m_limit_client_body_size = 0;
-	this->m_default_error_page.clear();
+
 }
 
 /* ************************************************************************** */
@@ -60,14 +82,19 @@ Server& Server::operator=(const Server& obj)
 {
 	if (this == &obj)
 		return (*this);
-	this->m_server_name = obj.get_m_server_name();
-	this->m_host = obj.get_m_host();
-	this->m_port = obj.get_m_port();
-	this->m_fd = obj.get_m_fd();
-	this->m_request_uri_limit_size = obj.get_m_request_uri_limit_size();
-	this->m_request_header_limit_size = obj.get_m_request_header_limit_size();
-	this->m_limit_client_body_size = obj.get_m_limit_client_body_size();
-	this->m_default_error_page = obj.get_m_default_error_page();
+	m_manager = obj.m_manager;
+	m_server_name = obj.m_server_name;
+	m_host = obj.m_host;
+	m_port = obj.m_port;
+	m_fd = obj.m_fd;
+	m_request_uri_limit_size = obj.m_request_uri_limit_size;
+	m_request_header_limit_size = obj.m_request_header_limit_size;
+	m_limit_client_body_size = obj.m_limit_client_body_size;
+	m_default_error_page = obj.m_default_error_page;
+	m_config = obj.m_config;
+	m_locations = obj.m_locations;
+	m_connections = obj.m_connections;
+	m_responses = obj.m_responses;
 	/* overload= code */
 	return (*this);
 }
@@ -75,15 +102,15 @@ Server& Server::operator=(const Server& obj)
 std::ostream&
 operator<<(std::ostream& out, const Server& server)
 {
-	out << "" << server.get_m_server_name();
-	out << "" << server.get_m_host();
-	out << "" << server.get_m_port();
-	out << "" << server.get_m_fd();
-	out << "" << server.get_m_request_uri_limit_size();
-	out << "" << server.get_m_request_header_limit_size();
-	out << "" << server.get_m_limit_client_body_size();
-	out << "" << server.get_m_default_error_page();
-	out << "" << std::endl;
+	// out << "" << server.get_m_server_name();
+	// out << "" << server.get_m_host();
+	// out << "" << server.get_m_port();
+	// out << "" << server.get_m_fd();
+	// out << "" << server.get_m_request_uri_limit_size();
+	// out << "" << server.get_m_request_header_limit_size();
+	// out << "" << server.get_m_limit_client_body_size();
+	// out << "" << server.get_m_default_error_page();
+	// out << "" << std::endl;
 	return (out);
 }
 
@@ -91,27 +118,27 @@ operator<<(std::ostream& out, const Server& server)
 /* --------------------------------- GETTER --------------------------------- */
 /* ************************************************************************** */
 
-std::string Server::get_m_server_name() const { return (this->m_server_name); }
-std::string Server::get_m_host() const { return (this->m_host); }
-int Server::get_m_port() const { return (this->m_port); }
-int Server::get_m_fd() const { return (this->m_fd); }
-int Server::get_m_request_uri_limit_size() const { return (this->m_request_uri_limit_size); }
-int Server::get_m_request_header_limit_size() const { return (this->m_request_header_limit_size); }
-int Server::get_m_limit_client_body_size() const { return (this->m_limit_client_body_size); }
-std::string Server::get_m_default_error_page() const { return (this->m_default_error_page); }
+// std::string Server::get_m_server_name() const { return (this->m_server_name); }
+// std::string Server::get_m_host() const { return (this->m_host); }
+// int Server::get_m_port() const { return (this->m_port); }
+// int Server::get_m_fd() const { return (this->m_fd); }
+// int Server::get_m_request_uri_limit_size() const { return (this->m_request_uri_limit_size); }
+// int Server::get_m_request_header_limit_size() const { return (this->m_request_header_limit_size); }
+// int Server::get_m_limit_client_body_size() const { return (this->m_limit_client_body_size); }
+// std::string Server::get_m_default_error_page() const { return (this->m_default_error_page); }
 
 /* ************************************************************************** */
 /* --------------------------------- SETTER --------------------------------- */
 /* ************************************************************************** */
 
-void Server::set_m_server_name(std::string server_name) { this->m_server_name = server_name; }
-void Server::set_m_host(std::string host) { this->m_host = host; }
-void Server::set_m_port(int port) { this->m_port = port; }
-void Server::set_m_fd(int fd) { this->m_fd = fd; }
-void Server::set_m_request_uri_limit_size(int request_uri_limit_size) { this->m_request_uri_limit_size = request_uri_limit_size; }
-void Server::set_m_request_header_limit_size(int request_header_limit_size) { this->m_request_header_limit_size = request_header_limit_size; }
-void Server::set_m_limit_client_body_size(int limit_client_body_size) { this->m_limit_client_body_size = limit_client_body_size; }
-void Server::set_m_default_error_page(std::string default_error_page) { this->m_default_error_page = default_error_page; }
+// void Server::set_m_server_name(std::string server_name) { this->m_server_name = server_name; }
+// void Server::set_m_host(std::string host) { this->m_host = host; }
+// void Server::set_m_port(int port) { this->m_port = port; }
+// void Server::set_m_fd(int fd) { this->m_fd = fd; }
+// void Server::set_m_request_uri_limit_size(int request_uri_limit_size) { this->m_request_uri_limit_size = request_uri_limit_size; }
+// void Server::set_m_request_header_limit_size(int request_header_limit_size) { this->m_request_header_limit_size = request_header_limit_size; }
+// void Server::set_m_limit_client_body_size(int limit_client_body_size) { this->m_limit_client_body_size = limit_client_body_size; }
+// void Server::set_m_default_error_page(std::string default_error_page) { this->m_default_error_page = default_error_page; }
 
 /* ************************************************************************** */
 /* ------------------------------- EXCEPTION -------------------------------- */
@@ -122,3 +149,24 @@ void Server::set_m_default_error_page(std::string default_error_page) { this->m_
 /* ************************************************************************** */
 /* ---------------------------- MEMBER FUNCTION ----------------------------- */
 /* ************************************************************************** */
+
+// bool Server::hasException(int client_fd){}
+// int Server::closeConnection(int client_fd){}
+// int Server::isSendable(int client_fd){}
+// int Server::sendResponse(Response response){}
+// bool Server::hasRequest(int client_fd){}
+// Request Server::readRequest(int client_fd){}
+// int Server::solveRequest(Request request){}
+// int Server::executeGet(Request request){}
+// int Server::executeHead(Request request){}
+// int Server::executePut(Request request){}
+// int Server::executePost(Request request){}
+// int Server::executeDelete(Request request){}
+// int Server::executeOptions(Request request){}
+// int Server::executeTrace(Request request){}
+// char** Server::createCGIEnv(Request request){}
+// int Server::executeCGI(Request request){}
+// int Server::createResponse(int status){}
+// bool Server::hasNewConnection(){}
+// int Server::acceptNewConnection(){}
+// int Server::run(){}
